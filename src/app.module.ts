e6f71896151e6998ae169537b06eb09361ba656c
unknown <eze.rey92@gmail.com>
2021-11-16 23:39:28 -0300

    viendo la forma de conectar base de datos a heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,36 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+const isProduction = true;
 //comment
 @Module({
   imports: [
-    ConfigModule.forRoot(
-      {
-        envFilePath:[`.env.stage.${process.env.STAGE}`]
-      }),
-    TasksModule,TypeOrmModule.forRoot({type:"postgres",
-  host:"localhost",
-  port:5432,
-  username:"postgres",
-  password:"matuzay32",
-  database:"taks-management",
-  autoLoadEntities:true,
-  synchronize:true
-
-
-  }), AuthModule],
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+    TasksModule,
+  /*   TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'matuzay32',
+      database: 'taks-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }), */
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-52-208-229-228.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'oxbvsrnbequngq',
+      password: '6eb6682dcef1b198005f9b798656b51c794d0a8fc6dedc7c92246edf4bd55333',
+      database: 'dgmhik9k8b7bb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
